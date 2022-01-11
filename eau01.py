8c366f6e11ef6d7c2ecc0b63820ba2b74913d1bb
Nabilbmk9 <nabil.bmk9@gmail.com>
2022-01-11 08:31:35 +0100

    Combinaisons de 2 nombres

--- eau01.py
+++ eau01.py
@@ -6,22 +6,24 @@ liste2 = []
 liste3 = []
 tour = 0
 
-#Créer une liste 
+#Créer une liste avec toute les combinaison de 2 chiffres
 for a in chiffre:
     for b in chiffre:
         liste.append([str(a) + str(b)])
 
+# Refaire le 1 mais 2 fois en retirant les doublons
 for c in range(len(liste)):
     for d in range(len(liste)):
         if liste[c] != liste[d] :
             liste2.append(liste[c] + liste[d])
             
+# Trier le tout et enlever les doublons
 for e in range(len(liste2)):
     liste2[e].sort()
     if liste2[e] not in liste3:
         liste3.append(' '.join(liste2[e]))
         
-
+# Print en ligne avec une virgule
 for f in liste3:
     for g in f:
         print(g, end="")
